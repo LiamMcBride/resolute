0334aa8724506b91b5fc34327d167202b989497f
Liam McBride <liammcbride@Liams-MacBook-Air.local>
2023-01-20 23:38:34 -0500

    changed some colors of stuff and added a second testObj

--- Frontend/Extension/app.js
+++ Frontend/Extension/app.js
@@ -10,19 +10,30 @@ let testObj = {
         "duration": 5
     },
 }
+let testObj2 = {
+    "current": {
+        "course": "Systems",
+        "type": "Project",
+        "start-date-time": "01-12-2023 07:15:00",
+        "duration": 4002,
+    },
+}
 
 function startApp(){
     let current = document.getElementById("current-div");
     let html = createEntry(testObj);
-    current.innerHTML = html;
+    current.innerHTML += html;
+    current = document.getElementById("current-div");
+    html = createEntry(testObj2);
+    current.innerHTML += html;
 }
 
 function createEntry(obj){
     return `
     <div class="entry">
-        <div>${testObj["current"]["course"]}</div>
-        <div>${testObj["current"]["type"]}</div>
-        <div>${testObj["current"]["duration"]}</div>
+        <div>${obj["current"]["course"]}</div>
+        <div>${obj["current"]["type"]}</div>
+        <div>${obj["current"]["duration"]}</div>
         <button class="pause-btn"></button>
         <button class="stop-btn"></button>
     </div>
